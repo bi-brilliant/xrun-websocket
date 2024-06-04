e85e0352d256feb586b8439d456b228d780aa3cc
Bilal Jibrilliant <bilaljibrilliant@gmail.com>
2024-06-04 13:23:49 +0700

    Remove unused code

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const server = require("http").createServer(app);
-// const Websocket = require("ws");
-
-// const wss = new Websocket.Server({ server: server });
-
-// wss.on("connection", function connection(ws) {
-//   ws.send("Welcome new client");
-
-//   ws.on("error", console.error);
-
-//   ws.on("message", function message(data) {
-//     console.log("received: %s", data);
-//     ws.send("Got ur msg its: " + data);
-
-//     wss.clients.forEach(function each(client) {
-//       if (client.readyState === Websocket.OPEN) {
-//         client.send(data);
-//       }
-//     });
-//   });
-// });
-
-// app.get("/", (req, res) => res.send("Hello World"));
-
-// server.listen(3005, () => console.log(`Lisening on port :3005`));
-
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
